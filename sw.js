014c5d6d9571c6e376b39cd3b3f33b9e4bacf6c5
Sergio <k9w9wqjzkb@privaterelay.appleid.com>
2026-02-21 19:15:17 +0100

    Versione_2

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-// Service Worker – cache con versionamento + aggiornamenti puliti
-const CACHE_NAME = 'iMieiEsami-v3';
-
-const ASSETS = [
+const CACHE_NAME = 'v1';
+const assets = [
   './',
   './index.html',
   './style.css',
@@ -10,42 +8,14 @@ const ASSETS = [
   './assets/icon.png'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys
-        .filter((k) => k !== CACHE_NAME)
-        .map((k) => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  // Network-first per HTML (per aggiornamenti più rapidi), cache-first per gli altri asset
-  if (req.mode === 'navigate') {
-    e.respondWith(
-      fetch(req)
-        .then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE_NAME).then((c) => c.put('./index.html', copy));
-          return res;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
